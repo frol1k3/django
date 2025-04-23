e632b8ef61437e905cda534762d71c435ad7ecf6
frol1k3 <alex_frolov24042007@mail.ru>
2025-04-23 20:46:35 +0300

    Построили отображение моделей для админа

--- products/models.py
+++ products/models.py
@@ -4,6 +4,12 @@ class Author(models.Model):
     name = models.CharField("Имя", max_length=100)
     bio = models.TextField("Биография")
 
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
 
 class Book(models.Model):
     title = models.CharField("Название", max_length=70)
@@ -15,3 +21,10 @@ class Book(models.Model):
     date_edit = models.DateTimeField("Дата редактирования", auto_now=True)
 
     authors = models.ManyToManyField(Author, related_name="books", verbose_name="Автор(ы)")
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
