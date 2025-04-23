a24943bc2584e9192eeda9a0f77ef22578a836ec
frol1k3 <alex_frolov24042007@mail.ru>
2025-04-23 20:17:30 +0300

    Создали модели автора и книги

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField("Имя", max_length=100)
+    bio = models.TextField("Биография")
+
+
+class Book(models.Model):
+    title = models.CharField("Название", max_length=70)
+    desc = models.TextField("Описание")
+    price = models.FloatField("Цена")
+    date_public = models.DateField("Дата публикации")
+
+    date_create = models.DateTimeField("Дата добавления", auto_now_add=True)
+    date_edit = models.DateTimeField("Дата редактирования", auto_now=True)
+
+    authors = models.ManyToManyField(Author, related_name="books", verbose_name="Автор(ы)")
