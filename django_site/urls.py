e5cbadca304f0b346840bfa0478824935945719a
frol1k3 <alex_frolov24042007@mail.ru>
2025-04-23 21:13:36 +0300

    Добавлена главная страница

--- django_site/urls.py
+++ django_site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django_site.views import index_redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("products/", include("products.urls")),
+    path("", index_redirect)
 ]
