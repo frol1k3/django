5960d62e2ba4b654d27dbf24e0fbaf8237d50a23
frol1k3 <alex_frolov24042007@mail.ru>
2025-04-30 19:15:11 +0300

    изменил функцию представления главной страницы на класс

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from products.views import index, catalog
+from products.views import index, catalog, IndexListView
 
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", IndexListView.as_view, name="index"),
     path("catalog/", catalog, name="catalog"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from products.models import Book
+from django.views.generic import ListView
 
-def index(req):
-    books = Book.objects.all()
-    return render(req, "index.html", {"books" : books})
+class IndexListView (ListView):
+    model = Book 
+    template_name = "index.html"
+    context_object_name = "books"
+
+#def index(req):
+#   books = Book.objects.all()
+#   return render(req, "index.html", {"books" : books})
 
 def catalog(req):
     return render(req, "catalog.html")
