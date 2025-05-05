a62f6e9d8cd9077cc65866c4e8611e7e42971d01
frol1k3 <alex_frolov24042007@mail.ru>
2025-05-05 21:13:07 +0300

    создана страница авторизации

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-
+from user.views import *
 
 urlpatterns = [
-
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("registration/", RegistrationView.as_view(), name="registration"),
+    path("account/", AccountView.as_view(), name="account"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserLoginView(LoginView):
+    template_name = "login.html"
+    success_url = "/user/account"
+
+class RegistrationView(CreateView):
+    form_class = UserCreationForm
+    template_name = "registration.html"
+    success_url = "/user/account"
+
+class AccountView(DetailView):
+    model = User
+    template_name = "account.html"
+    context_object_name = "account"
+
+class UserLogoutVeiw(LogoutView):
+    pass
